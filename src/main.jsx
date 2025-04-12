746bfafe13a6f6b2e29486fc0560941cc3afed68
Rizz Fe <46671452+airizf11@users.noreply.github.com>
2025-04-12 15:56:10 +0700

    Fix src/main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap-icons/font/bootstrap-icons.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
-import App from './App.jsx';
+import App from './app.jsx';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
